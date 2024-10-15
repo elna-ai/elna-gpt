d9b81be5f1f93391f8277c36fd1635d9a2250133
Hash-if-vs <hashifvs0075@gmail.com>
2024-10-15 17:05:20 +0530

    stable memory with wasi-polyfill

--- ic_llama3/src/ic_llama3_backend/src/lib.rs
+++ ic_llama3/src/ic_llama3_backend/src/lib.rs
@@ -1,15 +1,39 @@
-mod inference;
 mod storage;
 
+use ic_stable_structures::{
+    memory_manager::{MemoryId, MemoryManager},
+    DefaultMemoryImpl,
+};
+use std::cell::RefCell;
+
+const WASI_MEMORY_ID: MemoryId = MemoryId::new(0);
 const MODEL_FILE: &str = "model.onnx";
 
+thread_local! {
+    // The memory manager is used for simulating multiple memories.
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+}
+
+#[ic_cdk::init]
+fn init() {
+    // Initialize ic_wasi_polyfill with a memory manager.
+    let wasi_memory = MEMORY_MANAGER.with(|m| m.borrow().get(WASI_MEMORY_ID));
+    ic_wasi_polyfill::init_with_memory(&[0u8; 32], &[], wasi_memory);
+}
+
+#[ic_cdk::post_upgrade]
+fn post_upgrade() {
+    // Reinitialize ic_wasi_polyfill after canister upgrade.
+    let wasi_memory = MEMORY_MANAGER.with(|m| m.borrow().get(WASI_MEMORY_ID));
+    ic_wasi_polyfill::init_with_memory(&[0u8; 32], &[], wasi_memory);
+}
+
+/// Appends the given chunk to the model file.
+/// This is used for incremental chunk uploading of large files.
 #[ic_cdk::update]
 fn append_model_bytes(bytes: Vec<u8>) {
     storage::append_bytes(MODEL_FILE, bytes);
-}
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
 }
 
 ic_cdk::export_candid!();
