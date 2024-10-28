038cc7cf869cbc3bd0ee90c6388e47550e1f24cc
Alex Jolly <alexjolly28@gmail.com>
2024-10-28 12:28:56 +0530

    build script modified

--- ic_gpt2/scripts/build.sh
+++ ic_gpt2/scripts/build.sh
@@ -2,9 +2,9 @@
 set -ex
 MODULE=$1
 export RUSTFLAGS=$RUSTFLAGS' -C target-feature=+simd128'
-cargo build --release --target=wasm32-wasi
+cargo build -p $MODULE --release --target=wasm32-wasi
 wasi2ic ./target/wasm32-wasi/release/$MODULE.wasm ./target/wasm32-wasi/release/$MODULE.wasm
 wasm-opt -Os -o ./target/wasm32-wasi/release/$MODULE.wasm \
          ./target/wasm32-wasi/release/$MODULE.wasm
 
-# candid-extractor "target/wasm32-wasi/release/ic_embedding_backend.wasm" > "src/ic-embedding-backend/ic-embedding-backend.did"
+candid-extractor "target/wasm32-wasi/release/$MODULE.wasm" > "src/$MODULE/$MODULE.did"
